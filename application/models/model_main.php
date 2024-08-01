d05dff79c62816c617f997eeb61d1d114b01a520
Abdul Lathief Permana <alatathiefpermana@gmail.com>
2024-08-01 09:22:50 +0700

    config update

--- application/models/model_main.php
+++ application/models/model_main.php
@@ -6,4 +6,19 @@ class model_main extends CI_Model{
     function insert_data($table,$array){
         $this->db->insert($table,$array);
     }
+
+    function update_data($id, $table, $array){
+        $this->db->where('id',$id);
+        $this->db->update($table, $array);
+    }
+
+    function data_result($table, $where, $where_is){
+        if(!empty($where)){
+            $this->db->where($where);
+        }
+        if(!empty($where_is)){
+            $this->db->where($where_is);
+        }
+        return $this->db->get($table);
+    }
 }
