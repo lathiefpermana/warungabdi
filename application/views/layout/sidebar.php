c223fce59d9b747a5f3b7747a48c9c89fb41aba9
Abdul Lathief Permana <alatathiefpermana@gmail.com>
2024-08-26 11:18:16 +0700

    update sidebar

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -29,7 +29,7 @@
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a class="sidebar-link" href="<?= base_url('produk/daftar_harga'); ?>" aria-expanded="false">
+                    <a class="sidebar-link" href="<?= base_url('daftar_harga'); ?>" aria-expanded="false">
                         <span><i class="ti ti-tag"></i></span>
                         <span class="hide-menu">Daftar Harga</span>
                     </a>
