a5d935472eefe54397afe7a49125b1773bcc5d3d
Abdul Lathief Permana <alatathiefpermana@gmail.com>
2024-09-04 09:59:14 +0700

    add stok awal

---
+++ application/controllers/stok_awal.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class stok_awal extends CI_Controller {
+
+    public function session_data()
+    {
+        $lisensi = $this->input->cookie('lisensi-warung-abdi');
+        $status_login = $this->session->userdata('status_login');
+        if(empty($status_login)){
+            redirect(base_url('autentikasi/login'));
+        }
+    }
+
+    public function index()
+    {
+        $data = $this->session_data();
+        $data['bulan'] = date('m');
+        $data['tahun'] = date('Y');
+        $data['content'] = 'stok_awal/index';
+        $this->load->view('layout',$data);
+    }
+
+}
