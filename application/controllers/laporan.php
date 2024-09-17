ef707711c2058d961f7158a86a17c5251cebe201
Abdul Lathief Permana <alatathiefpermana@gmail.com>
2024-09-17 16:19:15 +0700

    laporan cash flow

---
+++ application/controllers/laporan.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class laporan extends CI_Controller {
+
+    public function session_data()
+    {
+        $lisensi = $this->input->cookie('lisensi-warung-abdi');
+        $status_login = $this->session->userdata('status_login');
+        if(empty($status_login)){
+            redirect(base_url('autentikasi/login'));
+        }
+    }
+    
+    //Laporan Penjualan
+
+    //Laporan Barang Masuk
+
+    //Laporan cash Flow
+
+    function cash_flow()
+    {
+        $data = $this->session_data();
+        $bulan = date('m');
+        $tahun = date('Y');
+        $cash_flow = $this->model_main->data_result('cash_flow',array('month(tanggal)'=>$bulan, 'year(tanggal)'=>$tahun),'delete_by IS NULL');
+        $data['cashflow'] = $cash_flow->result();
+        $data['content'] = 'laporan/cash_flow';
+        $this->load->view('layout',$data);
+    }
+
+}
---
+++ application/views/laporan/cash_flow.php
@@ -0,0 +1,65 @@
+<div class="body-wrapper">
+    <div class="container-fluid">
+        <div class="card bg-info-subtle shadow-none position-relative overflow-hidden mb-4">
+            <div class="card-body px-4 py-3">
+                <div class="row align-items-center">
+                    <div class="col-9">
+                        <h4 class="fw-semibold mb-8">Laporan <i>Cash Flow</i></h4>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item">
+                                    <a class="text-muted text-decoration-none" href="<?= base_url(); ?>">Dasbor</a>
+                                </li>
+                                <li class="breadcrumb-item" aria-current="page">Laporan <i>Cash FLow</i></li>
+                            </ol>
+                        </nav>
+                    </div>
+                    <div class="col-3">
+                        <div class="text-center mb-n5">
+                            <img src="<?= base_url('assets/images/breadcrumb/ChatBc.png'); ?>" alt="" class="img-fluid mb-n4"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="datatables">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="multi_control-custom" class="table border table-striped table-bordered display text-nowrap" style="width: 100%">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Tanggal</th>
+                                            <th>Deskripsi</th>
+                                            <th>Pemasukan</th>
+                                            <th>Pengeluaran</th>
+                                            <th>Saldo</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php foreach($cashflow as $key): ?>
+                                        <?php endforeach; ?>
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Tanggal</th>
+                                            <th>Deskripsi</th>
+                                            <th>Pemasukan</th>
+                                            <th>Pengeluaran</th>
+                                            <th>Saldo</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
